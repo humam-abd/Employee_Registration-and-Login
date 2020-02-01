ef72f901e99becfeb1735db2d0b2a66ab6aa5b14
HAbd97 <humamabdullah@outlook.com>
2020-02-01 22:58:23 +0530

    auth edit

--- Frontend_React/src/GetWT.js
+++ Frontend_React/src/GetWT.js
@@ -26,7 +26,7 @@ class DetailWT extends Component {
     var mytoken = this.state.DToken;
     var options = {
       headers: {
-        'Authorization':`Bearer : ${mytoken}`
+        'Authorization':`Bearer ${mytoken}`
       }
     };
     var param = {
